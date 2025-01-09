9e52d1139f9ac59a5534d16d0fc369dca1604c23
成成0v0 <chengcheng@miao.ms>
2025-01-09 11:58:54 +0800

    feat(server): 实现动态加载 TOML 配置

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, net::SocketAddr, process, sync::Arc};
+use std::{collections::HashMap, fs, net::SocketAddr, process, sync::Arc};
 
 use axum::{
     middleware,
@@ -7,6 +7,7 @@ use axum::{
 };
 use clogger::*;
 use layers::rate_limit::{self, FixedTimeWindowByIpConfig};
+use serde::Deserialize;
 use sqlx::{MySql, MySqlPool, Pool};
 use tokio::{net::TcpListener, sync::Mutex};
 use tower::limit::ConcurrencyLimitLayer;
@@ -18,8 +19,59 @@ mod layers;
 
 #[tokio::main]
 async fn main() {
+    // 配置的结构体
+    #[derive(Deserialize)]
+    struct Config {
+        #[allow(dead_code)]
+        version: String,
+        server: ServerConfig,
+        database: DatabaseConfig,
+    }
+    #[derive(Deserialize)]
+    struct DatabaseConfig {
+        r#type: String,
+        host: String,
+        port: u16,
+        name: String,
+        username: String,
+        password: String,
+    }
+    #[derive(Deserialize)]
+    struct ServerConfig {
+        log_output_file: String,
+        listener_addr: String,
+        limit: ServerLimitConfig,
+    }
+    #[derive(Deserialize)]
+    struct ServerLimitConfig {
+        max_concurrency: usize,
+        add_comment: AddCommentLimitConfig,
+    }
+    #[derive(Deserialize)]
+    struct AddCommentLimitConfig {
+        limit: u32,
+        window_size: u64,
+    }
+
+    // 获取配置文件内容
+    let config_origin = match fs::read_to_string("config.toml") {
+        Ok(content) => content,
+        Err(e) => {
+            println!("[Error] 无法读取配置文件: {}", e);
+            process::exit(1); // 直接终止程序
+        }
+    };
+    // 解析配置
+    let config: Config = match toml::from_str(&config_origin) {
+        Ok(config) => config,
+        Err(e) => {
+            println!("[Error] 无法解析 TOML 配置: {}", e);
+            process::exit(1); // 直接终止程序
+        }
+    };
+
     // 初始化 CLogger
-    init_clogger("/dev/null"); // 将输出写入黑洞
+    init_clogger(&config.server.log_output_file);
 
     // 频率限制中间件的配置结构体
     struct RateLimitConfigs {
@@ -30,8 +82,8 @@ async fn main() {
     let rate_limit_configs = RateLimitConfigs {
         add_comment: Arc::new(FixedTimeWindowByIpConfig {
             requests: Mutex::new(HashMap::new()),
-            limit: 1,
-            window_size: 120,
+            limit: config.server.limit.add_comment.limit,
+            window_size: config.server.limit.add_comment.window_size,
         }),
     };
 
@@ -63,8 +115,16 @@ async fn main() {
     };
 
     // 连接数据库
-    let database_url = "mysql://test:thisisapasswd@127.0.0.1/picocmt";
-    let db_pool = match MySqlPool::connect(database_url).await {
+    let database_url = format!(
+        "{}://{}:{}@{}:{}/{}",
+        config.database.r#type,
+        config.database.username,
+        config.database.password,
+        config.database.host,
+        config.database.port,
+        config.database.name
+    );
+    let db_pool = match MySqlPool::connect(&database_url).await {
         Ok(pool) => {
             c_log!(format!("数据库连接成功: {}", database_url));
             pool
@@ -81,11 +141,15 @@ async fn main() {
         .merge(routes.general)
         // 添加全局中间件
         .layer(TraceLayer::new_for_http()) // 日志跟踪
-        .layer(ConcurrencyLimitLayer::new(80)) // 全局并发限制
+        .layer(ConcurrencyLimitLayer::new(
+            config.server.limit.max_concurrency,
+        )) // 全局并发限制
         .with_state(db_pool); // 共享数据库池
 
     // 监听地址
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = TcpListener::bind(config.server.listener_addr)
+        .await
+        .unwrap();
     c_log!(format!("正在监听地址: {}", listener.local_addr().unwrap()));
 
     // 启动服务
