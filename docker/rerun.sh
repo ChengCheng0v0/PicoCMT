4b004b7542f3d13dd546c798f7989fd8595bdbc2
成成0v0 <chengcheng@miao.ms>
2025-01-10 05:45:30 +0800

    feat(docker): 新增快速创建和重新创建 Docker 容器的 Shell 脚本

---
+++ docker/rerun.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+NAME=${NAME:-picocmt}
+TAG=${TAG:-latest}
+ONLY_SERVER=${ONLY_SERVER:-0}
+PUBLISH_SERVER_PORT=${PUBLISH_SERVER_PORT:-3000}
+PUBLISH_CLIENT_RES_PORT=${PUBLISH_CLIENT_RES_PORT:-80}
+CONFIG_PATH=${CONFIG_PATH:-/etc/picocmt/config.toml}
+
+docker rm ${NAME}
+docker run --name ${NAME} --mount type=bind,source=${CONFIG_PATH},target=/app/server/config.toml -e ONLY_SERVER=${ONLY_SERVER} --publish ${PUBLISH_SERVER_PORT}:3000 --publish ${PUBLISH_CLIENT_RES_PORT}:80 picocmt:${TAG}
+
+# Example
+# PUBLISH_SERVER_PORT=3001 PUBLISH_CLIENT_RES_PORT=81 ./rerun.sh
