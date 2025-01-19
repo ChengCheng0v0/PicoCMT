5a71f0e97d6b4ad6888d31ff0bf466d2ec8cff96
成成0v0 <chengcheng@miao.ms>
2025-01-19 19:39:21 +0800

    feat(docker): 移除静态资源服务

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,9 @@ NAME=${NAME:-picocmt}
 TAG=${TAG:-latest}
 ONLY_SERVER=${ONLY_SERVER:-0}
 PUBLISH_SERVER_PORT=${PUBLISH_SERVER_PORT:-3000}
-PUBLISH_CLIENT_RES_PORT=${PUBLISH_CLIENT_RES_PORT:-80}
 CONFIG_PATH=${CONFIG_PATH:-/etc/picocmt/config.toml}
 
-docker run --name ${NAME} --mount type=bind,source=${CONFIG_PATH},target=/app/server/config.toml -e ONLY_SERVER=${ONLY_SERVER} --publish ${PUBLISH_SERVER_PORT}:3000 --publish ${PUBLISH_CLIENT_RES_PORT}:80 chengcheng0v0/picocmt:${TAG}
+docker run --name ${NAME} --mount type=bind,source=${CONFIG_PATH},target=/app/server/config.toml -e ONLY_SERVER=${ONLY_SERVER} --publish ${PUBLISH_SERVER_PORT}:3000 chengcheng0v0/picocmt:${TAG}
 
 # Example
-# PUBLISH_SERVER_PORT=3001 PUBLISH_CLIENT_RES_PORT=81 ./run.sh
+# PUBLISH_SERVER_PORT=3001 ./run.sh
